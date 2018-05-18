f24b79ef1e28e672e4195b20daf7fc0dd7fce150
yiditheawe1 <yiditheawe1@gmail.com>
2018-05-17 22:06:59 -0300

    first commit

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickResponse(View v){
+        EditText edit =(EditText) findViewById(R.id.editText);
+        TextView fd=findViewById(R.id.field);
+        fd.setText(edit.getText().toString());
     }
 }
